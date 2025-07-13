5d166d75ed88e2017967ebb5215d239d7af9bac0
Marcos <marcostulli@hotmail.com>
2025-07-14 00:05:44 +0200

    Edit next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
-import type { NextConfig } from 'next';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
 };
 
 export default nextConfig;
