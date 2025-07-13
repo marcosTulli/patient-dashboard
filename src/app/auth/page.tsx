f4fcfa8c0ba52c4b4828c4a8edc4914815a6097a
Marcos <marcostulli@hotmail.com>
2025-07-14 01:52:57 +0200

    Create auth page

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,8 +1,56 @@
 'use client';
-import React from 'react';
 
-const AuthPage = () => {
-  return <div>AUTH</div>;
-};
+import {
+  Box,
+  Card,
+  CardContent,
+  Typography,
+  Tabs,
+  TabList,
+  Tab,
+  TabPanel,
+} from '@mui/joy';
 
-export default AuthPage;
+import * as React from 'react';
+
+export default function AuthPage() {
+  const [mode, setMode] = React.useState<'login' | 'signup'>('login');
+
+  return (
+    <Box>
+      <Card>
+        <CardContent>
+          <Typography
+            level="h4"
+            textAlign="center"
+            mb={2}
+            textColor="text.primary"
+          >
+            TITLE
+          </Typography>
+          <Tabs
+            value={mode}
+            onChange={(_, val) => setMode(val as 'login' | 'signup')}
+          >
+            {/* TABS */}
+            <TabList>
+              <Tab value="login">Login</Tab>
+              <Tab value="signup">Sign Up</Tab>
+            </TabList>
+
+            {/* LOGIN */}
+
+            <TabPanel value="login">
+              <form>LOGIN FORM</form>
+            </TabPanel>
+
+            {/* SIGNUP */}
+            <TabPanel value="signup">
+              <form>SIGN IN FORM</form>
+            </TabPanel>
+          </Tabs>
+        </CardContent>
+      </Card>
+    </Box>
+  );
+}
