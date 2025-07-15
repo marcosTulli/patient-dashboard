16e9dc7024a915f9d365cdb3b1586f0e39a2e165
Marcos <marcostulli@hotmail.com>
2025-07-15 22:57:56 +0200

    Create store and hook for dialogs

---
+++ src/store/overlays/useDialogStore.ts
@@ -0,0 +1,33 @@
+import { create } from 'zustand';
+
+export const dialogs = {
+  create: 'isOpenCreateDialog',
+  deactivate: 'isOpenDeactivateDialog',
+  edit: 'isOpenEditDialog',
+  alert: 'isOpenAlertDialog',
+  remove: 'isOpenRemoveFormItemDialog',
+  holidayRuleEdit: 'isOpenEditHolidayRuleDialog',
+} as const;
+
+type DialogKeys = keyof typeof dialogs;
+type DialogStateKeys = typeof dialogs[DialogKeys];
+
+type DialogState = {
+  [K in DialogStateKeys]: boolean;
+} & {
+  toggle: (params: { dialog: DialogStateKeys }) => void;
+};
+
+const initialState = Object.values(dialogs).reduce((state, key) => {
+  state[key] = false;
+  return state;
+}, {} as Record<DialogStateKeys, boolean>);
+
+export const useDialogsStore = create<DialogState>((set) => ({
+  ...initialState,
+  toggle: ({ dialog }) =>
+    set((state) => ({
+      [dialog]: !state[dialog],
+    })),
+}));
+
